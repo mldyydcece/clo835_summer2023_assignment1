49503f75a33e67d9bbe33c30159014d617fc220b
Meloudy Yodico <125516086+mldyydcece@users.noreply.github.com>
2023-06-04 23:28:33 -0400

    Add files via upload

---
+++ terraform_code (2)/terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Meloudy Yodico"
+    "App"   = "App_Image Color"
+    "Project" = "Assignment1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "myodico"
+}
